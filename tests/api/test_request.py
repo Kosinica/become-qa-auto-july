8b4de023a68280c01b799938aef70bcb424800ba
Kosinica <kosinica@gmail.com>
2022-08-16 18:45:19 +0300

    tests are added

--- tests/api/test_request.py
+++ tests/api/test_request.py
@@ -1,6 +1,20 @@
 import requests
 
+def test_http_status_code200():
+    r = requests.get('https://api.github.com/zen')
+   # print(r.__dict__)
+    assert r.status_code == 200
 
-def test_http_code200():
+def test_user_exists():
     r = requests.get('https://api.github.com/users/defunkt')
-    print(r.__dict__)
+   # print(r.__dict__)
+
+    assert r.json()['login'] == 'defunkt'
+    assert r.json()['id'] == 2
+
+def test_user_non_exists():
+    r = requests.get('https://api.github.com/users/hjhjhjhjhh')
+    
+    assert r.status_code == 404
+    assert r.json()['message'] == 'Not Found'
+    
